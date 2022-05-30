e0a6c5cc483a5820e7f81caf15f32d68e44db135
chkp-cdiaz <101901491+chkp-cdiaz@users.noreply.github.com>
2022-05-30 14:45:03 -0500

    Update anothersensiblefile.py

--- anothersensiblefile.py
+++ anothersensiblefile.py
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "us-east-2"
   access_key = "AKIA2H7U475L74XE3TLM"
-  secret_key = "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"
+  secret_key = "x4svoqxieG3DA14gKvIwJ5qTlB7GI0W+KaBRxf1c"
 }
 © 2022 GitHub, Inc.
