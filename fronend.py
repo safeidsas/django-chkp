dd36f738f3e90a24f5913865dbe2d2e1e55f8a63
chkp-cdiaz <101901491+chkp-cdiaz@users.noreply.github.com>
2022-06-01 16:55:55 -0500

    Create fronend.py

---
+++ fronend.py
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-2"
+  access_key = "AKIA2H7U475L74XE3Tkl"
+  secret_key = "x4svoqxieG3DA14gKvIwJ5qTlB7GI0W+KaBRxfSO"
+}
+
+sdjkaslkjdhasjkl;dnajskl
+sasdjkhasjkdlhaskjl
+
+asdasljkdhjasjk
